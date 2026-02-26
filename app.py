605b7d1342e1b20dbdd425fe32eb0175bcb39260
kamushkinael <igg7171@mail.ru>
2026-02-26 11:32:23 +0300

    opa

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import os, redis, json
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
 
-# Настройки пуупупупупуп
+# Чтобы Flask видел реальные IP за Nginx/Gunicorn
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
+
+# Настройа пупупупуп
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -14,9 +18,11 @@ class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
 
+# тут содаются таблицы когда мы бахаем воркер
 with app.app_context():
     db.create_all()
 
+# тут пути для запросов
 @app.route('/items', methods=['GET'])
 def get_items():
     cached = cache.get('items_list')
@@ -31,24 +37,27 @@ def get_items():
 @app.route('/items', methods=['POST'])
 def create_item():
     data = request.get_json()
+    if not data or 'name' not in data:
+        return jsonify({"error": "Name is required"}), 400
+        
     new_item = Item(name=data['name'])
     db.session.add(new_item)
     db.session.commit()
     cache.delete('items_list')
-    return jsonify({"status": "created"}), 201
+    return jsonify({"status": "created", "id": new_item.id}), 201
 
 @app.route('/items/<int:id>', methods=['PUT'])
 def update_item(id):
     item = Item.query.get_or_404(id)
     data = request.get_json()
     
-    if 'name' in data:
+    if data and 'name' in data:
         item.name = data['name']
         db.session.commit()
         cache.delete('items_list')
         return jsonify({"status": "updated"}), 200
     
-    return jsonify({"error": "Name is required"}), 400
+    return jsonify({"error": "Invalid data"}), 400
 
 @app.route('/items/<int:id>', methods=['DELETE'])
 def delete_item(id):
@@ -57,6 +66,3 @@ def delete_item(id):
     db.session.commit()
     cache.delete('items_list')
     return jsonify({"status": "deleted"}), 200
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
